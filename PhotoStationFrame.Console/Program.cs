5524e988c9452a4dcb73716d0b42c8e3b2eb3e27
Alex Witkowski <witkowski@develappers.de>
2018-04-30 19:16:33 +0200

    Add project files.

---
+++ PhotoStationFrame.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PhotoStationFrame.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
