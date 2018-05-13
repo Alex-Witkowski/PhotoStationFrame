2e2ab34a52bfeaeb9a84cc0a17f53e8c860d5a9d
Alexander Witkowski <witkowski@develappers.de>
2018-05-13 22:00:18 +0200

    Added first BLE code. Nothing with logic yet - have to ckech in adapter supports server/virtual peripheral mode.

---
+++ PhotoStationFrame.Uwp/Bluetooth/BleExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+
+namespace PhotoStationFrame.Uwp.Bluetooth
+{
+    public static class BleExtensions
+    {
+        public static bool Success(this GattServiceProviderResult result)
+        {
+            return result.Error == BluetoothError.Success;
+        }
+
+        public static bool Success(this GattLocalCharacteristicResult result)
+        {
+            return result.Error == BluetoothError.Success;
+        }
+    }
+}
---
+++ PhotoStationFrame.Uwp/Bluetooth/BleServer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Devices.Bluetooth;
+
+namespace PhotoStationFrame.Uwp.Bluetooth
+{
+    public class BleServer
+    {
+        public async Task<bool> CheckPeripheralRoleSupportAsync()
+        {
+            // BT_Code: New for Creator's Update - Bluetooth adapter has properties of the local BT radio.
+            var localAdapter = await BluetoothAdapter.GetDefaultAsync();
+
+            if (localAdapter != null)
+            {
+                return localAdapter.IsPeripheralRoleSupported;
+            }
+            else
+            {
+                // Bluetooth is not turned on 
+                return false;
+            }
+        }
+    }
+}
